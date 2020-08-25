6ae7924951cf8d4ee77d64792a138210fddc950e
Jay Motka <jaymotka.216@gmail.com>
2020-08-25 14:13:38 +0530

    urls updated/added

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, HttpResponse
 
-def home(request):
-    return HttpResponse('Home')
+def blogHome(request):
+    return HttpResponse('Blog Home')
 
-def blog(request):
-    return HttpResponse('Blog')
+def blogPost(request, slug):
+    return HttpResponse(f'Blog Post{slug}')
 # Create your views here.
--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
+
+def home(request):
+    return HttpResponse('Home')
+
+def contact(request):
+    return HttpResponse('Contact')
+
+def about(request):
+    return HttpResponse('About Us')
 # Create your views here.
